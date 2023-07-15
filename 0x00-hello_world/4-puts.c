7fd0a7a4dd197f00784c37ea8eb84160f1e10271
clair <clairhany2017@gmail.com>
2023-07-15 09:09:56 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<studio.h>
+
+/**
+ * main-Entry point
+ *
+ * Description: c program that print with function puts 
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return(0);
+}
