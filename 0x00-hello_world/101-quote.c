9d59525d445043663d1aea5596ba0b0e2696ed26
clair <clairhany2017@gmail.com>
2023-07-15 22:22:51 -0700

    hh

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * DESCRIPTION: program that prints a line with standard error 
+ * Return: 1 (success)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",student);
+	return (1);
+
+}
+
