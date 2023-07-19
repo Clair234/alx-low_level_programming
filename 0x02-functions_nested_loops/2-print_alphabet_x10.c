515fbb3baabac09d043aa8414fe24e4747fd58e2
clair <clairhany2017@gmail.com>
2023-07-18 19:02:04 -0700

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ *print_alphabet from a - z 10 times
+ *Return: 0 success
+ */
+void print_alphabet_x10(void)
+{
+int line, ch;
+
+for (line = 0; line <= 9; line++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
+}
