302e2fbcc1b481c9581a7e7ffabb8c2cb08e4d20
clair <clairhany2017@gmail.com>
2023-07-19 15:58:05 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			 * place the first digit if its two number
 			 */
 			if (prod <= 9)
-				_putchar(',');
+				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48); /*get the first digit*/
 					_putchar((prod % 10) + 48); /*get the second digit*/
