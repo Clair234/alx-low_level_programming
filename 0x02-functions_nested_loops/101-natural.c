2e08f058c1320ae9e201048e704b3dcc2ac200b6
clair <clairhany2017@gmail.com>
2023-07-19 18:28:38 -0700

    k

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main,h"
+
+/**
+ * main - entry poin
+ *
+ * Description: computes the sum of all 3 oe 5 bellow 1024
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
