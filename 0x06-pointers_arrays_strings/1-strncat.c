62a361b780b9b82fb5a63d92d4f116d118daf808
clair <clairhany2017@gmail.com>
2023-07-29 14:14:25 -0700

    l

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,27 +5,28 @@ include "main.h"
  *
  * @dest: pointer to destination input
  * @src: pointer to source input
- * @n: most number of bytes from @src
- *
- * Return: @dest
+ * @n: input value
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int i;
+	int j;
 
-	c = 0;
-	/*find size of dest array*/
-	while (dest[c])
-		c++;
+	i = 0;
 
-	/**
-	 * src does not need to be null terminated
-	 * if it contains n or more bytes
-	 */
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	/*null terminate dest*/
-	dest[c + i] = '\0';
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
