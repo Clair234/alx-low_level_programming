26deafeac0fbbb46bf7f32960d209453b43b0bec
clair <clairhany2017@gmail.com>
2023-07-29 22:26:24 -0700

    l

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,9 +11,9 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[];
+	char data1[] =
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[];
+	char datarot[] =
 	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
 		for (i = 0; s[i] != '\0'; i++)
 		{
