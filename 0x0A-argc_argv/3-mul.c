2c5011f91ed635241277ec5727e68ac2e747fcb0
Clair <clairhany2017@gmail.com>
2023-08-09 18:23:25 -0700

    l

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+
+/**
+ * main - print sum of 2 numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[i]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
