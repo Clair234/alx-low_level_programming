95a828c965f122b440811cb16f963da6d08df489
Clair <clairhany2017@gmail.com>
2023-08-16 17:57:24 -0700

    l

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,15 +1,18 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: the string name
- * @f: the printing function pointer
+ * array_iterator - maps an array through a func pointer
+ * @array: the int array
+ * @size: the array size
+ * @action: function pointer
  *
  * Return: void
  */
 
-void print_name(char *name, void (*f)(char *))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (name && f)
-		f(name);
+        int *end = array + size - 1;
+        if (array && size && action)
+                while (array <= end)
+                        action(*array++);
 }
