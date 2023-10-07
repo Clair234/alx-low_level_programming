6a600c3ebc0f5a59850aed4d7e92f3b85549b022
clairhany <clairhany2017@gmail.com>
2023-10-07 14:30:37 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the result of the multiplication
+ * @argc: int
+ * @argv: list
+ * Return: 0-sucess, non-zero-fail
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("error\n");
+			return (1);
+	}
+	return (0);
+}
