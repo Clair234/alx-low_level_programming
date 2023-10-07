9041956d699789c21e31a3d2117d41f49ce8465b
clairhany <clairhany2017@gmail.com>
2023-10-07 16:22:07 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,31 +10,31 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int i, mincents = 0, money = atoi(argv[1]);
-		int cents[] = {25, 10, 5, 2, 1};
+	int num, i, res;
+	int cents[] = {25, 10, 5, 2, 1};
 
-		for (i = 0; i < 5; i++)
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	res = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= cents[j])
 		{
-			if (money >= cents[i])
-			{
-				mincents += money / cents[i];
-				if (money % cents[i] == 0)
-				{
-					break;
-				}
-			}
+			res++;
+			num = cents[j];
 		}
 	}
-	printf("%d\n", mincents);
-
-else
-{
-	printf("Error\n");
-	return (1);
-}
+	printf("%d\n", res);
 	return (0);
 }
-
-
