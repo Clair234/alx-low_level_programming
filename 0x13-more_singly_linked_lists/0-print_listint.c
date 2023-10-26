e430e0096cef7eb27335495ab51816101e1d0c04
clair234 <clairhany2017@gmail.com>
2023-10-25 17:13:09 -0700

    l

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,14 @@
 size_t print_listint(const listint_t *h)
 {
 
-	size_t i = 0;
-
-	while (h)
+	size_t count;
+	
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
-	return (i);
+	return (count);
 }
